0b4c01af11b3cb4744463f7e8482babb6727b509
liuyishi <352887191@qq.com>
2025-11-01 00:45:25 +0800

    1. 解决 favicon.ico 的不展示的问题

--- sudoku-h5/config.js
+++ sudoku-h5/config.js
@@ -1,17 +1,16 @@
 // 应用全局配置
 module.exports = {
     // baseUrl: 'http://localhost:8080',
-    baseUrl: 'https://quanzhinaifen.aixcode.cn/api',
     // wsUrl: 'ws://localhost:8080'
     // 应用信息
     appInfo: {
         // 应用名称
-        name: "全脂奶粉防伪查询",
+        name: "纯粹数独",
         // 应用版本
         version: "1.0.0",
         // 官方网站
         site_url: "http://localhost",
         // 应用描述
-        description: "全脂奶粉防伪查询"
+        description: "一个零广告、轻量级的纯粹数独，专注于最本真的数字推理体验。"
     }
 }
